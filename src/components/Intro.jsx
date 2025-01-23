b6a15b1a8d9ecfe0f63f73e6c2f4ac62a4a3df3e
raonmama <47728604+sy3264@users.noreply.github.com>
2025-01-23 11:40:36 +0900

    3.intro 커밋

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,10 +1,50 @@
 import React from 'react'
+import about from '../assets/img/imgabout.png';
+// import about2 from '../assets/img/imgabout2.png';
+
+const introText = {
+    title:"port developer",
+    desc:["talent is","found at the end of the","effort"]
+}
 
 const Intro = () => {
   return (
-    <div>
-      Intro
-    </div>
+    <section id='intro'>
+        <div className="intro_inner">
+            <h1 className='intro_title'>
+               {introText.title}
+            </h1>
+            <div className="intro_lines" aria-hidden='true'>
+                <span className='line'></span>
+                <span className='line'></span>
+                <span className='line'></span>
+                <span className='line'></span>
+                <span className='line'></span>
+                <span className='line'></span>
+                <span className='line'></span>
+            </div>
+            <div className="intro_text">
+                <div className="text">
+                    <div>{introText.desc[0]}</div>
+                    <div>{introText.desc[1]}</div>
+                    <div>{introText.desc[2]}</div>
+                </div>
+                <div className="img">
+                    <img src={about} alt="이미지넣기" />
+                    {/* <img src={about2} alt="이미지넣기" /> */}
+                </div>
+            </div>
+            <div className="intro_lines bottom" aria-hidden='true'>
+                <span className='line'></span>
+                <span className='line'></span>
+                <span className='line'></span>
+                <span className='line'></span>
+                <span className='line'></span>
+                <span className='line'></span>
+                <span className='line'></span>
+            </div>
+        </div>
+    </section>
   )
 }
 
